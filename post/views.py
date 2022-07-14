a3010d096a4ff65660ae556acf55758752299f75
Lucas Mariano <lucas.svmariano@gmail.com>
2022-07-14 16:45:47 -0300

    alterações css

--- post/views.py
+++ post/views.py
@@ -1,8 +1,10 @@
+from pickle import GET
 from django.shortcuts import render
 from django.views.generic import DetailView
 from django.views.generic.edit import CreateView, DeleteView
 from .models import Post
 from django.urls import reverse_lazy
+
 
 def post_list(request):
     posts = Post.objects.all()
@@ -18,9 +20,12 @@ class PostDetailView(DetailView): # new
 class PostCreateView(CreateView): # new
     model = Post
     template_name = 'post/post_new.html'
-    fields = '__all__'
+    fields = ['user','postagem', 'date_posted']
 
 class PostDeleteView(DeleteView): # new
     model = Post
     template_name = 'post/post_delete.html'
     success_url = reverse_lazy('post_list')
+
+
+
