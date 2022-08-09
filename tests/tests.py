db4b302b9f62c82eebf1d2976b4600ac1f564067
Lucas Mariano <lucas.svmariano@gmail.com>
2022-08-09 14:03:30 -0300

    first test add

--- tests/tests.py
+++ tests/tests.py
@@ -1,2 +1,12 @@
+from django.test import TestCase
+from django.urls import reverse
+
+
 def test_qualquer_coisa():
     pass
+
+
+class LoginURLsTest(TestCase):
+    def test_login_home_url_is_correct(self):
+        login_url = reverse('login')
+        self.assertEqual(login_url, '/login/')
