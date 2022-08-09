801a36bead5f2e65070f7c272fd18eee17341401
Lucas Mariano <lucas.svmariano@gmail.com>
2022-08-09 14:26:28 -0300

    second test add

--- tests/tests.py
+++ tests/tests.py
@@ -8,5 +8,11 @@ def test_qualquer_coisa():
 
 class LoginURLsTest(TestCase):
     def test_login_home_url_is_correct(self):
-        login_url = reverse('login')
-        self.assertEqual(login_url, '/login/')
+        url = reverse('login')
+        self.assertEqual(url, '/login/')
+
+
+class PostURLsTest(TestCase):
+    def test_post_postDetail_url_is_correct(self):
+        url = reverse('post_detail', kwargs={'pk': 19})
+        self.assertEqual(url, '/logado/logado/19/')
